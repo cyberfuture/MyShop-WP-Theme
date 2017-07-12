35d28de08f5e2b1a5ba3898e56e89d108e6f5b49
Sandra <sandra@konechy.com>
2017-07-11 21:28:23 -0700

    Update gulp

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+require('./gulp/tasks/styles');
+/* require('./devStuff/gulp/tasks/watch');
+require('./devStuff/gulp/tasks/sprite');
+require('./devStuff/gulp/tasks/scripts');
+require('./devStuff/gulp/tasks/modernizr');
+require('./devStuff/gulp/tasks/build'); */
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php echo bloginfo('template_url'); ?>/css/foundation.css">
     <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url'); ?>">
   </head>
   <body>
